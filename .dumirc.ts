51fb05666fccb7b11355dae4a0c8602356f1877f
wangxingkang <wang_xingkang@qq.com>
2023-10-18 18:31:30 +0800

    docs: update dumi config

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,6 +1,28 @@
 import { defineConfig } from 'dumi';
 
+const logo = 'https://cdn.jsdelivr.net/gh/wangxingkang/pictures@latest/imgs/sensoro-design.svg';
+
+const base = '/custom-scrollbar/';
+const isProd = process.env.NODE_ENV === 'production';
+
+const prodConfig = isProd
+  ? defineConfig({
+      ssr: {},
+      mfsu: false,
+    })
+  : defineConfig({});
+
 export default defineConfig({
-  html2sketch: {},
-  mfsu: false,
+  base,
+  publicPath: base,
+  themeConfig: {
+    name: 'Scrollbar',
+    logo,
+    socialLinks: {
+      github: 'https://github.com/pansyjs/custom-scrollbar',
+    },
+  },
+  favicons: [logo],
+  hash: true,
+  ...prodConfig,
 })
